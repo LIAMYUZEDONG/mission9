dfa25dd35ae3a00e5eb11e70dc169624fcd7c101
LIAMYUZEDONG <yyyzzddd@gmail.com>
2022-03-08 19:19:18 -0700

    m9

---
+++ Mission 9 Assignment/Views/Shared/Components/CartSummary/CartSummaryViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookProject.Models;
+namespace BookProject.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket basketServices)
+        {
+            basket = basketServices;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
